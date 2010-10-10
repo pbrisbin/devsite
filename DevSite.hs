181235b5787adeefa16817b12588ac5c7eb16a6a
patrick brisbin <pbrisbin@gmail.com>
2010-10-10 00:23:08 -0400

    began atom feed, stuck reading to the Day type

--- DevSite.hs
+++ DevSite.hs
@@ -6,12 +6,15 @@ module Main where
 
 import Yesod
 import Yesod.Helpers.Static
+import Yesod.Helpers.AtomFeed
 import System.Directory
 import qualified Text.Hamlet as H
 import qualified Text.Cassius as C
 import qualified Data.ByteString.Lazy as L
 
+import Data.Time
 import Data.Char (toLower)
+import Data.ByteString.UTF8 (toString)
 
 import DataTypes
 import Posts
@@ -28,6 +31,7 @@ mkYesod "DevSite" [$parseRoutes|
 /posts/#String PostR    GET
 /tags          TagsR    GET
 /tags/#String  TagR     GET
+/feed          FeedR    GET
 /favicon.ico   FaviconR GET
 /robots.txt    RobotsR  GET
 /static        StaticR Static getStatic
@@ -151,6 +155,33 @@ getFaviconR = sendFile "image/x-icon" "favicon.ico"
 
 getRobotsR :: Handler RepPlain
 getRobotsR = return $ RepPlain $ toContent "User-agent: *"
+
+
+getFeedR :: Handler RepHtml
+getFeedR = notFound
+
+--do
+--    atomFeed AtomFeed
+--        { atomTitle = "New posts on pbrisbin dot com"
+--        , atomLinkSelf = FeedR
+--        , atomLinkHome = RootR
+--        , atomUpdated = mostRecent
+--        , atomEntries = map readAtomEntry allPosts
+--        }
+--        where
+--            mostRecent = let date = read . postDate $ head allPosts :: Day
+--                         in UTCTime date $ secondsToDiffTime 0
+--
+--            readAtomEntry post = AtomFeedEntry
+--                { atomEntryLink    = PostR $ postSlug post
+--                , atomEntryUpdated = readDate post
+--                , atomEntryTitle   = postTitle post
+--                , atomEntryContent = string $ postDescr post
+--                }
+--                where
+--                    readDate p = let date = read $ postDate p :: Day
+--                                 in UTCTime date $ secondsToDiffTime 0
+--                               
 
 -- }}}
 
