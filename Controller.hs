06dee1ffb098df3aec9daecca2dcacd6a67672d4
patrick brisbin <pbrisbin@gmail.com>
2010-10-19 21:32:36 -0400

    dont need to import so much

--- Controller.hs
+++ Controller.hs
@@ -8,19 +8,15 @@
 -- Stability   :  unstable
 -- Portability :  unportable
 --
--- Imports my entire Site and defines one method 'withServer' to be used
--- when creating the WAI app.
+-- Imports my Site and defines one method 'withServer' to be used when
+-- creating the WAI app.
 --
 -------------------------------------------------------------------------------
 module Controller (withServer) where
 
 import Yesod
 import DevSite
-import Posts
-import Layouts
-import Templates
 import Handlers
-import qualified Settings as S
 
 -- | Instantiate the Yesod route types
 mkYesodDispatch "DevSite" resourcesDevSite
