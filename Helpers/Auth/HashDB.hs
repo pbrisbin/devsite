3480c89463d4ea4c31704b85f5ce5a6f104a8a20
patrick brisbin <pbrisbin@gmail.com>
2011-01-11 10:03:17 -0500

    remove unneeded logic in hashdb auth plugin

--- Helpers/Auth/HashDB.hs
+++ Helpers/Auth/HashDB.hs
@@ -16,10 +16,42 @@
 -- their user id's and a sha1 hash of their password will already be
 -- stored.
 --
+-- Example usage:
+--
+-- > -- import the function
+-- > import Helpers.Auth.HashDB
+-- >
+-- > -- make sure you have an auth route
+-- > mkYesodData "MyApp" [$parseRoutes|
+-- > / RootR GET
+-- > /auth AuthR Auth getAuth
+-- > |]
+-- >
+-- >
+-- > -- make your app an instance of YesodAuth using this plugin
+-- > instance YesodAuth MyApp where
+-- >    type AuthId MyApp = UserId
+-- >
+-- >    loginDest _  = RootR
+-- >    logoutDest _ = RootR
+-- >    getAuthId    = getAuthIdHashDB AuthR 
+-- >    showAuthId _ = showIntegral
+-- >    readAuthId _ = readIntegral
+-- >    authPlugins  = [authHashDB]
+-- >
+-- >
+-- > -- include the migration function in site startup
+-- > withServer :: (Application -> IO a) -> IO a
+-- > withServer f = withConnectionPool $ \p -> do
+-- >     runSqlPool (runMigration migrateUsers) p
+-- >     let h = DevSite p
+--
+-- Your app must be an instance of YesodPersist and the username and
+-- hashed-passwords must be added manually to the database.
+--
 -- > echo -n 'MyPassword' | sha1sum
 --
--- can be used to get the hash which should be inserted in the database
--- before using this in your webapp.
+-- can be used to get the hash from the commandline.
 --
 -------------------------------------------------------------------------------
 module Helpers.Auth.HashDB
@@ -32,13 +64,11 @@ module Helpers.Auth.HashDB
 import Yesod
 import Yesod.Helpers.Auth
 
-import Control.Applicative ((<$>),(<*>))
+import Control.Applicative         ((<$>), (<*>))
 import Data.ByteString.Lazy.Char8  (pack)
 import Data.Digest.Pure.SHA        (sha1, showDigest)
 import Database.Persist.TH         (share2)
 import Database.Persist.GenericSql (mkMigrate)
-
-import System.IO
 
 -- | Computer the sha1 of a string and return it as a string
 sha1String :: String -> String
@@ -47,13 +77,9 @@ sha1String = showDigest . sha1 . pack
 -- | Generate data base instances for a valid user
 share2 mkPersist (mkMigrate "migrateUsers") [$persist|
 User
-    idUser   String Eq
+    username String Eq
     password String
-    UniqueUser idUser
-Ident
-    ident String Asc
-    user UserId Eq
-    UniqueIdent ident
+    UniqueUser username
 |]
 
 -- | Given a (user,password) in plaintext, validate them against the
@@ -72,6 +98,7 @@ validateUser (user,password) = runDB (getBy $ UniqueUser user) >>= \dbUser ->
 login :: AuthRoute
 login = PluginR "hashdb" ["login"]
 
+-- | Handle the login form
 postLoginR :: (YesodAuth y,
                YesodPersist y, 
                PersistBackend (YesodDB y (GHandler Auth y)))
@@ -82,51 +109,39 @@ postLoginR = do
         <*> stringInput "password"
 
     isValid <- validateUser (user,password)
+
     if isValid
         then setCreds True $ Creds "hashdb" user []
         else do
-            setMessage $ string "Invalid username/password"
+            setMessage $ [$hamlet| %em invalid username/password |]
             toMaster <- getRouteToMaster
             redirect RedirectTemporary $ toMaster LoginR
 
--- | Use this as getAuthId in your instance declaration by passing your
---   AuthR data type
+-- | A drop in for the getAuthId method of your YesodAuth instance which
+--   can be used if authHashDB is the only plugin in use.
 getAuthIdHashDB :: (Key User ~ AuthId master,
                     PersistBackend (YesodDB master (GHandler sub master)),
                     YesodPersist master,
                     YesodAuth master)
-                => (AuthRoute -> Route master)
-                -> Creds m
+                => (AuthRoute -> Route master) -- ^ your site's Auth Route
+                -> Creds m                     -- ^ the creds argument
                 -> GHandler sub master (Maybe UserId)
 getAuthIdHashDB authR creds = do
     muid <- maybeAuth
-    x <- runDB $ getBy $ UniqueIdent $ credsIdent creds
-    case (x, muid) of
-        -- Logged in and identified
-        (Just _, Just (uid, _)) -> return $ Just uid
-
-        -- Identified but not logged in
-        (Just (_, i), Nothing) -> return $ Just $ identUser i
-
-        -- Logged in but not identified
-        (Nothing, Just (uid, _)) -> do
-            setMessage $ [$hamlet| %em identifier added |]
-            runDB $ insert $ Ident (credsIdent creds) uid
-            return $ Just uid
-
-        -- Not logged in and not identified
-        (Nothing, Nothing) -> do
-            -- is there already a user but with no identifier?
-            y <- runDB $ getBy $ UniqueUser (credsIdent creds)
-            case y of
-                Just (uid, _) -> do
-                    setMessage $ [$hamlet| %em identifier added |]
-                    runDB $ insert $ Ident (credsIdent creds) uid
-                    return $ Just uid
-                Nothing -> do
+    case muid of
+        -- user already authenticated
+        Just (uid, _) -> return $ Just uid
+        Nothing       -> do
+            x <- runDB $ getBy $ UniqueUser (credsIdent creds)
+            case x of
+                -- user exists
+                Just (uid, _) -> return $ Just uid
+                Nothing       -> do
                     setMessage $ [$hamlet| %em user not found |]
                     redirect RedirectTemporary $ authR LoginR
 
+-- | Prompt for username and password, validate that against a database
+--   which holds the username and a hash of the password
 authHashDB :: (YesodAuth y,
                YesodPersist y, 
                PersistBackend (YesodDB y (GHandler Auth y)))
