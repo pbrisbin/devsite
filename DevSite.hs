49c31ef910131745bd93badc3ef310112bfde914
patrick brisbin <pbrisbin@gmail.com>
2011-03-25 16:40:50 -0400

    analytics, why not?

--- DevSite.hs
+++ DevSite.hs
@@ -223,6 +223,17 @@ sideBar = do
                 $else
                     <li>
                         <a href="@{AuthR LoginR}">login
+
+        <script>
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-22304237-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
 |]
 
 
