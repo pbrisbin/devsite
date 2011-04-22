f6e0c65f187a516dcc7dde33bb2c04dff39a3e22
patrick brisbin <pbrisbin@gmail.com>
2011-04-22 12:34:40 -0400

    that was dumb

--- DevSite.hs
+++ DevSite.hs
@@ -98,6 +98,17 @@ instance Yesod DevSite where
                                     powered by 
                                     <a href="http://docs.yesodweb.com/">yesod
                                     \ - #{yesodVersion}
+
+                    <script>
+                        var _gaq = _gaq || [];
+                        _gaq.push(['_setAccount', 'UA-22304237-1']);
+                        _gaq.push(['_trackPageview']);
+
+                        (function() {
+                            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                        })();
             |]
 
 instance YesodBreadcrumbs DevSite where
@@ -213,18 +224,7 @@ sideBar = do
                 $else
                     <li>
                         <a href="@{AuthR LoginR}">login
-
-        <script>
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-22304237-1']);
-            _gaq.push(['_trackPageview']);
-
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-|]
+        |]
 
 loadDocuments :: Handler [Document]
 loadDocuments = do
