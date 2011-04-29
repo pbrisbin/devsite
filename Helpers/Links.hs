cb5a6f57dfd3401d960f44917c09df1ca0c432e9
patrick brisbin <pbrisbin@gmail.com>
2011-04-29 11:16:38 -0400

    fix bug in post handler; modify links logic to allow external links

--- Helpers/Links.hs
+++ Helpers/Links.hs
@@ -12,26 +12,30 @@ import DevSite
 import Model
 import qualified Data.Text as T
 
--- | A generalized internal link
+-- | Either an interal route or an external url
+data Destination m = Internal (Route m) | External T.Text
+
+-- | A generalized link
 data Link m = Link
-    { linkRoute :: Route m
+    { linkDest  :: Destination m
     , linkTitle :: T.Text
     , linkText  :: T.Text
     }
 
--- | How to display a general link
+-- | How to display a link
 showLink :: Link m -> GWidget s m ()
-showLink (Link r t x) = [hamlet|<a title="#{t}" href="@{r}">#{x}|]
+showLink (Link (Internal i) t x) = [hamlet|<a title="#{t}" href="@{i}">#{x}|]
+showLink (Link (External e) t x) = [hamlet|<a title="#{t}" href="#{e}">#{x}|]
 
 -- | Items that can be linked to within this app
 class Linkable a where
     link :: a -> Link DevSite
 
 instance Linkable Post where
-    link p = Link (PostR $ postSlug p) (postTitle p) (postTitle p)
+    link p = Link (Internal $ PostR $ postSlug p) (postTitle p) (postTitle p)
 
 instance Linkable Tag where
-    link t = Link (TagR $ tagName t) (tagName t) (tagName t)
+    link t = Link (Internal $ TagR $ tagName t) (tagName t) (tagName t)
 
 instance Linkable Document where
     link = link . post
@@ -39,7 +43,7 @@ instance Linkable Document where
 -- | This is unsafe but useful. It basically says any link to raw 
 --   text is assumed to be a tag. There is no assurance the tag exists
 instance Linkable T.Text where
-    link t = Link (TagR $ T.toLower t) t t
+    link t = Link (Internal $ TagR $ T.toLower t) t t
 
 -- | A convenience helper for links specific to this app
 linkTo :: Linkable a => a -> GWidget s DevSite ()
