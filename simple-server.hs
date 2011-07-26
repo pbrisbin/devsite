77395fe8ec3dba22625b3f83f6e9b93c51ebf262
patrick brisbin <pbrisbin@gmail.com>
2011-07-26 15:44:06 -0400

    add debug logging to simple server

--- simple-server.hs
+++ simple-server.hs
@@ -1,5 +1,10 @@
-import Controller
+import Controller (withServer)
+import System.IO (hPutStrLn, stderr)
+import Network.Wai.Middleware.Debug (debug)
 import Network.Wai.Handler.Warp (run)
 
 main :: IO ()
-main = putStrLn "Loaded" >> withServer (run 3000)
+main = do
+    let port = 3000
+    hPutStrLn stderr $ "Application launched, listening on port " ++ show port
+    withServer $ run port . debug
