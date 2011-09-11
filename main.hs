bb74865a63169c994242d4ca1322f90961b3e524
patrick brisbin <pbrisbin@gmail.com>
2011-09-10 20:56:59 -0400

    use scaffolded main

--- main.hs
+++ main.hs
@@ -1,23 +1,68 @@
+{-# LANGUAGE CPP, DeriveDataTypeable #-}
+import Settings (AppEnvironment(..), AppConfig(..), loadConfig)
 import Application (withDevSite)
-import Yesod.Logger (makeLogger)
 import Network.Wai.Handler.Warp (run)
-import System.Environment (getArgs)
+import System.Console.CmdArgs hiding (args)
+import Data.Char (toUpper, toLower)
 
-import Settings (AppConfig(..), AppEnvironment(..), loadConfig)
+#ifndef PRODUCTION
+import Network.Wai.Middleware.Debug (debugHandle)
+import Yesod.Logger (logString, logLazyText, flushLogger, makeLogger)
+#else
+import Yesod.Logger (makeLogger)
+#endif
 
 main :: IO ()
 main = do
-    l <- makeLogger
-    c <- loadConfig Production
-    p <- getArgsWithDefault $ appPort c
-    withDevSite c l $ run p
+    logger <- makeLogger
+    args   <- cmdArgs argConfig
+    env    <- getAppEnv args
+    config <- loadConfig env
+    let c = if port args /= 0 
+            then config { appPort = port args }
+            else config
+
+#if PRODUCTION
+    withDevSite c logger $ run (appPort c)
+#else
+    logString logger $ (show env) ++ " application launched, listening on port " ++ show (appPort c)
+    withDevSite c logger $ run (appPort c) . debugHandle (logHandle logger)
+    flushLogger logger
 
     where
-        getArgsWithDefault :: Int -> IO Int
-        getArgsWithDefault def = return . go =<< getArgs
+        logHandle logger msg = logLazyText logger msg >> flushLogger logger
+#endif
 
-            where
-                go :: [String] -> Int
-                go ("-p"    :portS:_) = read portS
-                go ("--port":portS:_) = read portS
-                go _                  = def
+data ArgConfig = ArgConfig
+    { environment :: String
+    , port        :: Int
+    } deriving (Show, Data, Typeable)
+
+argConfig :: ArgConfig
+argConfig = ArgConfig
+    { environment = def 
+        &= help ("application environment, one of: " ++ (foldl1 (\a b -> a ++ ", " ++ b) environments))
+        &= typ "ENVIRONMENT"
+    , port = def
+        &= typ "PORT"
+    }
+
+environments :: [String]
+environments = map ((map toLower) . show) ([minBound..maxBound] :: [AppEnvironment])
+
+-- | retrieve the -e environment option
+getAppEnv :: ArgConfig -> IO AppEnvironment
+getAppEnv cfg = do
+    let e = if environment cfg /= ""
+            then environment cfg
+            else
+#if PRODUCTION
+                "production"
+#else
+                "development"
+#endif
+    return $ read $ capitalize e
+
+    where
+        capitalize [] = []
+        capitalize (x:xs) = toUpper x : map toLower xs
