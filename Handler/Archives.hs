755bffef1035a248cc22995fe5a7d05922197292
patrick brisbin <pbrisbin@gmail.com>
2012-01-27 08:56:12 -0500

    put up a temp archive page

--- Handler/Archives.hs
+++ Handler/Archives.hs
@@ -1,9 +1,18 @@
 module Handler.Archives (getArchivesR) where
 
 import Import
+import Helpers.Post
 
 getArchivesR :: Handler RepHtml
 getArchivesR = do
+    posts <- runDB $ selectList [] [Desc PostDate]
+
     defaultLayout $ do
         setTitle "Archives"
         $(widgetFile "archives")
+
+    where
+        postWidget :: Post -> Widget
+        postWidget post = do
+            published <- liftIO $ postPublished post
+            $(widgetFile "post/_archive_row_item")
